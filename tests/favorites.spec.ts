3f960f80447486bfaa83016be8510b8846f9468e
Shyam k <kotechashyam1819@gmail.com>
2026-05-08 23:48:47 +0530

    add/remove methods for favorites

--- tests/favorites.spec.ts
+++ tests/favorites.spec.ts
@@ -2,7 +2,7 @@ import {test, expect} from '../fixtures/test.fixture'
 import { home } from '../pages/home.page'
 import { articleAPI } from '../api/article.api'
 
-test("Add/remove to favorites",async({page, apiContext})=>{
+test("Add and remove article from favorites",async({page, apiContext})=>{
    // const api = new articleAPI(apiContext)
     const title = "Discover Bondar Academy: Your Gateway to Efficient Learning"
    // await api.createArticle(title,'about','content')
@@ -21,12 +21,3 @@ test("Add/remove to favorites",async({page, apiContext})=>{
     expect(await home_page.get_favorites_count(title)).toBe(countBefore)
 })
 
-test("Remove from favorites",async({page})=>{
-    // const title:string = 'Mastering Knowledge with Self-Assessments: Identifying and Bridging Learning Gaps in Education'
-    // const home_page = new home(page)
-    // await home_page.open()
-    // const preCount = await home_page.get_favorites_count(title)
-    // await home_page.remove_from_favorites(title)
-    // const postCount = await home_page.get_favorites_count(title)
-    // expect(postCount).toEqual(preCount-1)
-})
